8254b432cd1d18846b4a838b86485fb8cdafc74f
Cun Gong <gcdevelop@163.com>
2020-08-01 21:09:38 +0800

    another method

--- 1114-PrintInOrder.h
+++ 1114-PrintInOrder.h
@@ -1,7 +1,7 @@
 class Foo {
   private:
     mutex mtx;
-    condition_variable cv_two, cv_three;
+    condition_variable cv;
     mutable int seq = 1;
     
   public:
@@ -10,30 +10,29 @@ class Foo {
     }
 
     void first(function<void()> printFirst) {
-        //lock_guard<mutex> lk(mtx);
         unique_lock<mutex> lk(mtx);
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
         seq = 2;
-        cv_two.notify_one();
+        cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
         unique_lock<mutex> lk(mtx);
         while (seq != 2) {
-            cv_two.wait(lk);
+            cv.wait(lk);
         }
        
         // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
         seq = 3;
-        cv_three.notify_one();
+        cv.notify_all();
     }
 
     void third(function<void()> printThird) {
         unique_lock<mutex> lk(mtx);
         while (seq != 3) {
-            cv_three.wait(lk);
+            cv.wait(lk);
         }
         
         // printThird() outputs "third". Do not change or remove this line.
