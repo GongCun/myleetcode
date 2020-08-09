e91e4b347ffb6db95727e979c7783f9cd0d2e9ff
Cun Gong <gcdevelop@163.com>
2020-08-09 20:44:23 +0800

    official

--- 415-AddStrings.h
+++ 415-AddStrings.h
@@ -1,41 +1,26 @@
 class Solution {
-  private:
-    inline int ctoi(char c) const {
-        return c - 48;
-    }
-
-    inline char itoc(int i) const {
-        return i + 48;
-    }
 
   public:
     string addStrings(string num1, string num2) {
-        int len;
+        int i, j;
         int carry = 0;
-        string s1, s2;
-        vector<char> v,r ;
+        vector<char> ans;
 
-        if (num1.length() < num2.length()) {
-            s1 = string(num2.length() - num1.length(), '0') + num1;
-            s2 = num2;
-        } else {
-            s1 = string(num1.length() - num2.length(), '0') + num2;
-            s2 = num1;
+        i = num1.length() - 1;
+        j = num2.length() - 1;
+
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = i >= 0 ? num1[i] - '0' : 0;
+            int y = j >= 0 ? num2[j] - '0' : 0;
+            int res = x + y + carry;
+            ans.push_back('0' + res % 10);
+            carry = res / 10;
+            i -= 1;
+            j -= 1;
         }
 
-        assert(s1.length() == s2.length());
-
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            int t = ctoi(s1[i]) + ctoi(s2[i]) + carry;
-            v.push_back(itoc(t % 10));
-            carry = (t > 9 ? 1 : 0);
-        }
-
-        if (carry > 0)
-            v.push_back(itoc(carry));
-
-        string res = string(v.rbegin(), v.rend());
-        return res;
+        return string(ans.rbegin(), ans.rend());
         
     }
 };
