b09ac144ed1cf367980ca2a7308db760519ca8a7
Cun Gong <gcdevelop@163.com>
2020-08-13 15:40:54 +0800

    pass & save

--- 133-CloneGraph.h
+++ 133-CloneGraph.h
@@ -2,47 +2,44 @@
 // Definition for a Node.
 class Node {
 public:
-int val;
-vector<Node*> neighbors;
+    int val;
+    vector<Node*> neighbors;
     
-Node() {
-val = 0;
-neighbors = vector<Node*>();
-}
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
     
-Node(int _val) {
-val = _val;
-neighbors = vector<Node*>();
-}
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
     
-Node(int _val, vector<Node*> _neighbors) {
-val = _val;
-neighbors = _neighbors;
-}
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 };
 */
 
 class Solution {
-  private:
-    map<int, bool> visited;
-    
-    void explore(Node *node, Node *n) {
-        visited[node->val] = true;
-        n->val = node->val;
-        /* cout << node->val << endl; */
-        for (vector<Node*>::iterator it = node->neighbors.begin(); it != node->neighbors.end(); ++it) {
-            if (!visited[(*it)->val]) {
-                Node *n2 = new Node();
-                n->neighbors.push_back(n2);
-                explore(*it, n2);
-            }
-        }
-    }
 
+  private:
+    unordered_map<Node*, Node*> visited;
+    
   public:
     Node* cloneGraph(Node* node) {
-        Node *n = new Node();
-        explore(node, n);
+        if (node == nullptr) return node;
+        
+        if (visited.find(node) != visited.end())
+            return visited[node];
+
+        Node *n = new Node(node->val);
+        visited[node] = n;
+
+        for (auto& i: node->neighbors) {
+            n->neighbors.push_back(cloneGraph(i));
+        }
 
         return n;
     }
